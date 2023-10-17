7a4f15bd4c1107eddd890faa59bdd4fa4ac50ec1
dhaaranisuriya2605@gmail.com <dhaaranisuriya2605@gmail.com>
2023-10-17 01:29:54 -0700

    connecting to the server(snake game app)

---
+++ play.js
@@ -0,0 +1,25 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "172.31.60.168",// IP address here, server ip address
+    port: 50541// PORT number here, server port
+  });
+
+  // conn.on("connect", () => {
+  //   conn.write("Connection established!"); //sends msg to server once connection established
+  // });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("data", (data) => {  //listens the event(msgs) from connected server
+    console.log("Server says: ", data);
+  });
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
