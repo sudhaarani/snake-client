9bc86c24c1fcd229a2313b348f51bfa3f3c631da
dhaaranisuriya2605@gmail.com <dhaaranisuriya2605@gmail.com>
2023-10-19 13:03:28 -0700

    committed play with calling func

--- play.js
+++ play.js
@@ -1,5 +1,9 @@
 
 const { connect } = require("./client");
+const { setupInput } = require("./input");
 
 console.log("Connecting ...");
-connect();
+const connObject = connect();
+//to send messages to the server we need connection object for setupInput func, so connection object is returned from connect func
+// in client.js is passed as argument to setupInput func here
+setupInput(connObject);
